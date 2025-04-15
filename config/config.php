4600634939cccc1612a10e13c3bb4fe73bff6867
awaisjuno <awaisjuno@gmail.com>
2025-04-16 02:27:49 +0500

    Remove Service Register, Upgrade Route Structure

--- config/config.php
+++ config/config.php
@@ -2,12 +2,7 @@
 
 $config = [
     'base_url' => 'http://localhost/colab',
-    'mode' => 'development', // can be 'development' or 'production'
-
-    // Register your service providers
-    'service_providers' => [
-        //\App\Providers\DatabaseServiceProvider::class,
-    ]
+    'mode' => 'production', // can be 'development' or 'production
 
 ];
 
--- config/routes.php
+++ config/routes.php
@@ -1,13 +1,9 @@
 <?php
 
-return [
-    '/gg' => [
-        'controller' => 'Pages',
-        'method' => 'index',
-        'middleware' => ['Authenticateff']
-    ],
-    'home' => [
-        'controller' => 'Pages',
-        'method' => 'show',
-    ]
-];
+use System\Routing;
+
+Routing::get('/', 'Pages@index');
+Routing::get('/home/{id}', 'Pages@home');
+Routing::post('/login', 'AuthController@login');
+Routing::put('/user/{id}', 'UserController@update', ['AuthMiddleware']);
+Routing::delete('/user/{id}', 'UserController@delete', ['AuthMiddleware']);
