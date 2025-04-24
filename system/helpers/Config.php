0167605139e144d7c75aaa32488468406ab4bbda
awaisjuno <awaisjuno@gmail.com>
2025-04-24 23:31:29 +0500

    Set Config Key Mapping

--- system/helpers/Config.php
+++ system/helpers/Config.php
@@ -5,24 +5,34 @@ namespace System\Helpers;
 class Config
 {
     protected static array $items = [];
+    protected static string $loadedFile = '';
 
-    protected static function load()
+    protected static function load(string $fileName)
     {
-        if (empty(self::$items)) {
-            $file = __DIR__ . '/../../config/database.php';
-            if (file_exists($file)) {
-                self::$items = include $file;
+        if (self::$loadedFile !== $fileName) {
+            if (pathinfo($fileName, PATHINFO_EXTENSION) !== 'php') {
+                $fileName .= '.php';
+            }
+
+            $filePath = __DIR__ . '/../../config/' . $fileName;
+
+            if (file_exists($filePath)) {
+                self::$items = include $filePath;
+                self::$loadedFile = $fileName;
+                // Optional: Debug
+                // echo "Loaded: $filePath\n";
             } else {
-                die("Config file not found at: $file");
+                die("Config file not found at: $filePath");
             }
         }
     }
 
     public static function get(string $key, $default = null)
     {
-        self::load();
-
         $segments = explode('.', $key);
+        $fileName = array_shift($segments);
+        self::load($fileName);
+
         $config = self::$items;
 
         foreach ($segments as $segment) {
@@ -35,3 +45,4 @@ class Config
         return $config;
     }
 }
+
