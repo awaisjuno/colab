ced7ae1bf4c8ad9c1813c606b962a16d1f7d7a5f
awaisjuno <awaisjuno@gmail.com>
2025-04-29 05:18:50 +0500

    Correct Project Name

--- index.php
+++ index.php
@@ -9,6 +9,20 @@ require_once ROOT_DIR . '/vendor/autoload.php';
 
 use System\Processing;
 
+// Report all PHP errors
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+// Catch Fatal Errors also
+register_shutdown_function(function() {
+    $error = error_get_last();
+    if ($error !== null) {
+        echo "<h1>Fatal Error</h1>";
+        echo "<pre>" . print_r($error, true) . "</pre>";
+    }
+});
+
+
 try {
     $processing = new Processing();
 } catch (Throwable $e) {
